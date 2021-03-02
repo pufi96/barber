6efa0f044190da86a68e394ef4938672ad59f470
pufi96 <58091599+pufi96@users.noreply.github.com>
2021-03-02 12:06:27 +0100

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -212,10 +212,7 @@ $(document).ready(function () {
     $('#Author').html(data.map((author)=>   `<div class="modal-dialog modal-dialog-centered" role="document">
                                                 <div class="modal-content">
                                                     <div class="modal-header">
-                                                        <h5 class="modal-title" id="exampleModalLongTitle">${author.title}</h5>
-                                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                        <span aria-hidden="true">&tnames;</span>
-                                                        </button>
+                                                        <h5 class="modal-title" id="exampleModalLongTitle">${author.title}</h5>                                                        
                                                     </div>
                                                     <div class="modal-body">
                                                         <div class="row">
