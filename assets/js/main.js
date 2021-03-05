0c1bd91e59d502ed1063e6a046ba2c441bc8667d
pufi96 <58091599+pufi96@users.noreply.github.com>
2021-03-05 12:29:17 +0100

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -384,10 +384,8 @@ $(document).ready(function () {
         }
     });
     $('.back-to-top').click(function () {
-        $('html, body').animate({
-            scrollTop: 0
-        }, 1500, 'easeInOutExpo');
-        return false;
+        document.body.scrollTop = 0; 
+        document.documentElement.scrollTop = 0;
     });
 
     // Sticky Navbar
