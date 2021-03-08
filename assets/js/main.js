34f5fef391724d5e694f075c90ec28dc42958bc2
pufi96 <58091599+pufi96@users.noreply.github.com>
2021-03-08 23:53:02 +0100

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,9 @@
 $(document).ready(function () {
 
-    //set default category on first load page
+    //set default category filter on first load page
     let categoriesFilter = getLocalStorage("categoriesFilter", {
         c1: true
     });
-
 
     //check if there is some value in local storage
     function getLocalStorage(key, defaultValue = null) {
@@ -76,51 +75,55 @@ $(document).ready(function () {
     //display category
     function displayCategories(data) {
         // render HTML
-        $('#category').html(data.map((category) => `<li data-category-id="${category.id}" class="${categoriesFilter[category.id] === true ? 'filter-active' : ''}">${category.name}</li>`).join(""));
+        // category filter hmtl
+        $('#category-filter').html(data.map((category) => `<li data-category-id="${category.id}" class="${categoriesFilter[category.id] === true ? 'filter-active-category' : ''}">${category.name}</li>`).join(""));
+        //category sort html
+
 
         // Attach event listeners to filters
-        $('#category li').on('click', function (e) {
+        $('#category-filter li').on('click', function (e) {
             const clickedElement = $(e.target);
             const clickedElementCategoryId = clickedElement.attr('data-category-id');
 
-			if (clickedElement.hasClass('filter-active')) {
-                if ($('#category li.filter-active').length <= 1) {
-                    $('#category li[data-category-id=c1]').addClass('filter-active');
-                    categoriesFilter.c1 = true;                
+            if (clickedElement.hasClass('filter-active-category')) {
+                if ($('#category-filter li.filter-active-category').length <= 1 ) {     
+                    $('#category-filter li[data-category-id=c1]').addClass('filter-active-category');
+                    categoriesFilter.c1 = true;                        
                 }
 
                 clickedElement.removeClass('filter-active-category');
                 delete categoriesFilter[clickedElementCategoryId];
                 //if all is only it can not be removed
                 if ($('#category-filter li.filter-active-category').length == 0) {
-                    $('#category-filter li[data-category-filter-id=c1]').addClass('filter-active-category');
+                    $('#category-filter li[data-category-id=c1]').addClass('filter-active-category');
                     categoriesFilter.c1 = true;                   
                     return;
                 }
                 getData('services', displayServices);
-                setLocalStorage("categoriesFilter", categoriesFilter)
+                setLocalStorage("categoriesFilter", categoriesFilter);
                 return;
             }
 
             if (clickedElementCategoryId == 'c1') {
-                $('#category li').removeClass('filter-active');
+                $('#category-filter li').removeClass('filter-active-category');
                 categoriesFilter = {
                     c1: true
                 };
             }
 
-            clickedElement.addClass('filter-active');
+            clickedElement.addClass('filter-active-category');
             categoriesFilter[clickedElementCategoryId] = true;
 
-            if ($('#category li.filter-active').length > 1) {
-                $('#category li[data-category-id=c1]').removeClass('filter-active');
+            if ($('#category-filter li.filter-active-category').length > 1) {
+                $('#category-filter li[data-category-id=c1]').removeClass('filter-active-category');
                 if (categoriesFilter.c1) {
                     delete categoriesFilter.c1;
                 }
             }
-
+            
             getData('services', displayServices);
-            setLocalStorage("categoriesFilter", categoriesFilter)
+            setLocalStorage("categoriesFilter", categoriesFilter);
+            setLocalStorage("sortFilter", sortFilter);
         });
 
         $('.sort-form #sort-select, .sort-form input[type=radio]').on('change', function (e) {
